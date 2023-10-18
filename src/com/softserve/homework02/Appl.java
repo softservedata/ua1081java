86daa368c138ef03bf54ff5a0d85c3aa2f4e631e
Jaroslav <garasym@gmail.com>
2023-10-18 20:02:48 +0300

    Homeworks01 updated

--- src/com/softserve/homework02/Appl.java
+++ src/com/softserve/homework02/Appl.java
@@ -2,6 +2,6 @@ package com.softserve.homework02;
 
 public class Appl {
     public static void main(String[] args) {
-        System.out.println("Appl");
+        System.out.println("Application");
     }
 }
