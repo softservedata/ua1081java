a69e502d8da93ae8baf1167a82703c0230951bcf
mtytysh <148088237+mtytysh@users.noreply.github.com>
2023-10-27 02:41:14 +0300

    Update Prtasks3.java

--- src/com/softserve/Practicaltasks/Prtasks3.java
+++ src/com/softserve/Practicaltasks/Prtasks3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Prtasks3 {
     public static void main(String[] args) {
        System.out.println("How are you?");
        String x;
