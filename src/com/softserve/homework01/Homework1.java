899dbfc5a83e6ad3282bda97ef82c0e1f7401ad6
mtytysh <148088237+mtytysh@users.noreply.github.com>
2023-10-27 02:41:49 +0300

    Update Homework1.java

--- src/com/softserve/homework01/Homework1.java
+++ src/com/softserve/homework01/Homework1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Homework1 {
     public static void main(String[] args) {
        System.out.println("Write radius of the Flower bed");
         Double x;
