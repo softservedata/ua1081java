d833eac549c294f68760eb2216894ce36b51864f
mtytysh <148088237+mtytysh@users.noreply.github.com>
2023-10-27 02:42:07 +0300

    Update Homework2.java

--- src/com/softserve/homework01/Homework2.java
+++ src/com/softserve/homework01/Homework2.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Homework2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        System.out.println("Hello, What is your name?");
