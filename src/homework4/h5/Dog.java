9cc74c71aed50439463d1181cdab8bdc5ba8450d
ZenukD <zenukddd@gmail.com>
2023-11-02 15:42:31 +0100

    refactoring Dog

--- src/homework4/h5/Dog.java
+++ src/homework4/h5/Dog.java
@@ -3,6 +3,8 @@ package homework4.h5;
 import java.util.Objects;
 
 public class Dog {
+
+
     public enum Breed {
         BULLDOG, SPANIEL, BEAGLE, ROTTWEILER;
     }
@@ -19,6 +21,10 @@ public class Dog {
 
     public String getName() {
         return name;
+    }
+
+    public Breed getBreed() {
+        return breed;
     }
 
     @Override
@@ -42,12 +48,25 @@ public class Dog {
         Dog dog1 = new Dog("Moon", Breed.BULLDOG, 2);
         Dog dog2 = new Dog("Sea", Breed.BEAGLE, 1);
         Dog dog3 = new Dog("Sun", Breed.ROTTWEILER, 3);
-        System.out.println(dog1.equals(dog2));
-        System.out.println(dog2.equals(dog3));
-        System.out.println(dog1.equals(dog3));
-
-        
 
 
-    }
-}
+
+        if (dog1.getAge() > dog2.getAge() && dog1.getAge() > dog3.getAge()) {
+            System.out.println(dog1.getName() + " it is oldest dog and has breed " + dog1.getBreed());
+        } else if (dog2.getAge() > dog1.getAge() && dog2.getAge() > dog3.getAge()) {
+            System.out.println(dog2.getName() + " it is oldest dog and has breed " + dog2.getBreed());
+        } else {
+            System.out.println(dog3.getName() + " it is oldest dog and has breed " + dog3.getBreed());
+        }
+
+
+                if ((dog1.getName().equals(dog2.getName())) || (dog1.getName().equals(dog3.getName())) || (dog2.getName()).equals(dog3.getName())) {
+                    System.out.println("The dogs have the same name");
+                } else {
+                    System.out.println("All names are unique");
+                }
+
+
+            }
+        }
+
