1b9aa43de20e3bb58b738e38cbc34a79f7fb4757
OlhenShu <olya.shutileva@gmail.com>
2023-11-07 02:23:32 +0200

    some comments to homework_01

--- src/com/softserve/homework01/HomeWork1.java
+++ src/com/softserve/homework01/HomeWork1.java
@@ -3,45 +3,48 @@ package com.softserve.homework01;
 import java.util.Scanner;
 
 public class HomeWork1 {
-    static double pi = 3.14;
+    static double pi = 3.14;//correct to use Math.PI
+    static Scanner sc = new Scanner(System.in);
 
-    public void circleCalculation(){
-        Scanner sc = new Scanner(System.in);
+    public static void circleCalculation() {
+
         System.out.println("input radius = ");
         double radius = sc.nextDouble();
         double perimeter = 2 * pi * radius;
-        double area = pi * Math.pow(radius,2);
-        System.out.println("perimeter = " + perimeter);
-        System.out.println("area = " + area);
+        double area = pi * Math.pow(radius, 2);
+        System.out.println("perimeter = " + (Math.round(perimeter * 100.0) / 100.0));//correct to use Math.round() or String.format("%.2f", perimeter)
+        System.out.print("area = " + (Math.round(area * 100.0) / 100.0));
+        sc.nextLine();//for cleaning the buffer
     }
 
-    public void inputInformationAboutPeople(){
-        Scanner sc = new Scanner(System.in);
+    public static void inputInformationAboutPeople() {
+
         System.out.println("What is your name?");
-        String name = sc.next();
+        String name = sc.nextLine();//more convenient to use nextLine() instead of next()
         System.out.println("Where do you live " + name + "?");
-        String address = sc.next();
-        System.out.println(name + " lives in " +  address);
+        String address = sc.nextLine();//more convenient to use nextLine() instead of next()
+        System.out.println(name + " lives in " + address);
+        sc.nextLine();//for cleaning the buffer
     }
 
-    public void calculateTheCostOfCalls(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input the tariff of a minute from the first country = ");
-        double tariff1 =sc.nextDouble();
-        System.out.println("Input the duration of the call = ");
-        double time1 =sc.nextDouble();
+    public static void calculateTheCostOfCalls() {
+
+        System.out.print("Input the tariff of a minute from the first country = ");
+        double tariff1 = sc.nextDouble();
+        System.out.print("Input the duration of the call = ");
+        double time1 = sc.nextDouble();
         double fullCost1 = tariff1 * time1;
         System.out.println("The cost of a call from the first country = " + fullCost1);
-        System.out.println("Input the tariff of a minute from the second country = ");
-        double tariff2 =sc.nextDouble();
-        System.out.println("Input the duration of the call = ");
-        double time2 =sc.nextDouble();
+        System.out.print("Input the tariff of a minute from the second country = ");
+        double tariff2 = sc.nextDouble();
+        System.out.print("Input the duration of the call = ");
+        double time2 = sc.nextDouble();
         double fullCost2 = tariff2 * time2;
         System.out.println("The cost of a call from the second country = " + fullCost2);
-        System.out.println("Input the tariff of a minute from the third country = ");
-        double tariff3 =sc.nextDouble();
-        System.out.println("Input the duration of the call = ");
-        double time3 =sc.nextDouble();
+        System.out.print("Input the tariff of a minute from the third country = ");
+        double tariff3 = sc.nextDouble();
+        System.out.print("Input the duration of the call = ");
+        double time3 = sc.nextDouble();
         double fullCost3 = tariff3 * time3;
         System.out.println("The cost of a call from the third country = " + fullCost3);
         double fullCost = fullCost1 + fullCost2 + fullCost3;
@@ -50,11 +53,11 @@ public class HomeWork1 {
 
 
     public static void main(String[] args) {
-       HomeWork1 homeWork1 = new HomeWork1();
-       homeWork1.circleCalculation();
-       System.out.println();
-       homeWork1.inputInformationAboutPeople();
-       System.out.println();
-       homeWork1.calculateTheCostOfCalls();
+       // HomeWork1 homeWork1 = new HomeWork1();// better to use static methods
+        HomeWork1.circleCalculation();
+        System.out.println();
+        HomeWork1.inputInformationAboutPeople();
+        System.out.println();
+        HomeWork1.calculateTheCostOfCalls();
     }
 }
