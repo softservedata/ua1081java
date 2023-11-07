87e47de2189b8b63b2f268fe9990a266a9a409cb
MaxymP <esperio033@gmail.com>
2023-11-07 23:20:38 +0200

    Practical_1

---
+++ src/Lesson3/Practical1.java
@@ -0,0 +1,35 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class Practical1 {
+
+    public static double getTotal(double num1, double num2) {
+        double sum = num1 + num2;
+        return sum;
+
+    }
+
+    public static double getAverage(double num1, double num2) {
+        double sum = (num1 + num2) / 2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter first num: ");
+        num1 = scan.nextInt();
+
+        System.out.println("Enter second num: ");
+        num2 = scan.nextInt();
+
+        System.out.println("The sum of: " + num1 + " and " + num2 + " is " + getTotal(num1, num2));
+        System.out.println("The average of: " + num1 + " and " + num2 + " is " + getAverage(num1, num2));
+    }
+
+
+}
