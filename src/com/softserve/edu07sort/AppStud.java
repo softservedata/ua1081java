c2a6611b81337829bbf463c08422049aaa8f12eb
Jaroslav <garasym@gmail.com>
2023-11-08 19:26:23 +0200

    Inner Classes. Clonable Interface

--- src/com/softserve/edu07sort/AppStud.java
+++ src/com/softserve/edu07sort/AppStud.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class AppStud {
 	public static void main(String[] args) {
-		// /*-
+		/*-
 		Student[] students = new Student[7];
 		students[0] = new Student("Oksana", 26);
 		students[1] = new Student("Bogdan", 27);
@@ -13,8 +13,8 @@ public class AppStud {
 		students[4] = new Student("Yarko", 21);
 		students[5] = new Student("Bogdan", 30);
 		students[6] = new Student("Bogdan", 22);
-		// */
-		/*
+		*/
+		// /*
 		List<Student> students = new ArrayList<>();
 		students.add(new Student("Oksana", 26));
 		students.add(new Student("Bogdan", 27));
@@ -23,19 +23,19 @@ public class AppStud {
 		students.add(new Student("Yarko", 21));
 		students.add(new Student("Bogdan", 30));
 		students.add(new Student("Bogdan", 22));
-		*/
+		// */
 		//String[] students = {"Oksana", "Bogdan", "Orest", "Ira", "Yarko"};
 		//
-		System.out.println("Original Arrays: " + Arrays.toString(students));
-		//System.out.println("Original Arrays: " + students);
+		//System.out.println("Original Arrays: " + Arrays.toString(students));
+		System.out.println("Original Arrays: " + students);
 		//
 		//Arrays.sort(students);
 		//Arrays.sort(students, new Student.ByDestName());
 		//Arrays.sort(students, new Student().new ByAge());
-		Arrays.sort(students, new Student.ByNameAndAge());
+		//Arrays.sort(students, new Student.ByNameAndAge());
 		//Arrays.sort(students, new Student.ByNameAndAge());
 		//
-		//Collections.sort(students);
+		Collections.sort(students);
 		//Collections.sort(students, new Student.ByNameAndAge());
 		//students.sort(new Student.ByDestName());
 		/*
@@ -47,8 +47,8 @@ public class AppStud {
 				}
 		);
 		*/
-		System.out.println("Sorted Arrays: " + Arrays.toString(students));
-		//System.out.println("Sorted Arrays: " + students);
+		//System.out.println("Sorted Arrays: " + Arrays.toString(students));
+		System.out.println("Sorted Arrays: " + students);
 		//
 	}
 }
