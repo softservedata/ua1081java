effb47a39c4c2b07272de7e7687c2f9ea36e85e4
nkondrat <nkondrat@softserveinc.com>
2023-11-12 00:43:35 +0200

    Add Homework 5.4

---
+++ src/com/softserve/homework05/homework/GuessTheNumber.java
@@ -0,0 +1,29 @@
+package com.softserve.homework05.homework;
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        int attempt = 1;
+        int minNumber = 0;
+        int maxNumber = 100;
+        int randomNumber = (int)Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+        //System.out.println(randomNumber);
+
+        System.out.print("I guessed a number from 0 to 100.\n" +
+                "Try to guess it!\n" +
+                "Please enter a number: ");
+        guess = scanner.nextInt();
+        while (guess != randomNumber) {
+            System.out.print(guess > randomNumber ? "Too high, try again! " : "Too low, try again! ");
+            attempt++;
+            guess = scanner.nextInt();
+        }
+
+        System.out.println("You guessed it!\n" +
+                "This is indeed the number " + randomNumber + ".\n" +
+                "You managed to guess it in " + attempt + " tries.");
+    }
+}
