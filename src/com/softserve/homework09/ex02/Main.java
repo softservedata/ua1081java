07e4ff5247be6283da8a18056724496e506ba3a4
OleksandrArtemenko <silentmoment2014@gmail.com>
2023-11-22 19:23:54 +0200

    small fixes hw09

--- src/com/softserve/homework09/ex02/Main.java
+++ src/com/softserve/homework09/ex02/Main.java
@@ -8,20 +8,22 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
+        double requiredAvg = 3.0;
         students.add(new Student("Bob", "Economics", 2, 3, 5, 4));
         students.add(new Student("Jane", "Banking", 4, 3.6, 4.7, 5));
         students.add(new Student("Alex", "Physics", 1, 2, 2.5, 2.8));
         students.add(new Student("Ben", "Physics", 4, 3, 2.7, 2.9));
 
         StudentAssessment.printStudents(students);
+        System.out.println();
         System.out.println("Please, input number of a course to display: ");
         int input = sc.nextInt();
 
         System.out.printf("Students studying at the course №%d:%n", input);
         StudentAssessment.printStudents(students, input);
         System.out.println();
-        System.out.println("Removal of students because of the average below 3.0:");
-        StudentAssessment.removeStudents(students, 3);
+        System.out.printf("Removal of students because of the average below %s:%n", requiredAvg);
+        StudentAssessment.removeStudents(students, requiredAvg);
         System.out.println();
         StudentAssessment.printStudents(students);
 
--- src/com/softserve/homework09/ex02/StudentAssessment.java
+++ src/com/softserve/homework09/ex02/StudentAssessment.java
@@ -1,22 +1,30 @@
 package com.softserve.homework09.ex02;
 
-import java.sql.SQLOutput;
 import java.util.Iterator;
 import java.util.List;
 
 public class StudentAssessment {
     static void printStudents(List<Student> students, int course) {
+        boolean courseExists = false;
         for (Student student : students) {
             if (student.getCourse() == course) {
                 System.out.println(student);
+                courseExists = true;
             }
+        }
+        if (!courseExists) {
+            System.out.println("There are no students of input course");
         }
     }
 
     static void printStudents(List<Student> students) {
-        System.out.println("The whole group is:");
-        for (Student student : students) {
-            System.out.println(student);
+        if (!students.isEmpty()) {
+            System.out.println("The whole group is:");
+            for (Student student : students) {
+                System.out.println(student);
+            }
+        } else {
+            System.out.println("There are no students in the group");
         }
     }
 
