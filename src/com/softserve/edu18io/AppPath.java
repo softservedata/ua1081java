5894a5157d4dad4145e214efc5601442ea0d1ae9
Jaroslav <garasym@gmail.com>
2023-12-06 21:16:36 +0200

    Paths.get(uri) fixed

--- src/com/softserve/edu18io/AppPath.java
+++ src/com/softserve/edu18io/AppPath.java
@@ -2,17 +2,31 @@ package com.softserve.edu18io;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class AppPath {
     public static void main(String[] args) throws Exception {
-        //String uribase = "https://softserve.academy///";
-        String uribase = "C://Intel//javaProjects///ua1081java///";
-        //URI uri = new URI(uribase);
-        //System.out.println("uri = " + uri);
-        //Path path = (Path) Paths.ge
-        Path path = (Path) Paths.get(uribase);
+        // /*
+        String uribase = "file:///C:/Tools/1/test.txt";
+        URI uri = new URI(uribase);
+        System.out.println("uri = " + uri);
+        Path path = (Path) Paths.get(uri);
+        //
+        //String uribase = "C://Intel//javaProjects///ua1081java///";
+        //Path path = (Path) Paths.get(uribase);
+        //
         System.out.println(path);
+        // */
+        //
+        /*
+        Path path = Paths.get(URI.create("file:///tutorial/Java/JavaFX/Topic.txt"));
+        System.out.println(path);
+        //
+        path = Paths.get(URI.create("file:///C:/tutorial/Java/JavaFX/Topic.txt"));
+        System.out.println(path);
+        */
     }
 }
